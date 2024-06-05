bcc2348b654463d2df88bcd9a00443ec705dfaf7
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2024-06-05 12:05:51 +0200

    Nav Bar created

--- src/lib/home/home.dart
+++ src/lib/home/home.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:adopte_1_candidat/constants.dart';
+import 'package:adopte_1_candidat/routes.dart';
+import 'package:adopte_1_candidat/redundancy/navigation_bar.dart';
 import 'bubbles_widget.dart';
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+       body: Stack(
         children: [
           BubblesWidget(bubbleCount: 6),
           // Add other widgets for your home page here
         ],
       ),
+      bottomNavigationBar: BottomNavBar(selectedIndex: 0),
     );
   }
 }
-
+ 
