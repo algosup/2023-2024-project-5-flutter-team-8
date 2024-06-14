b5d28ef37f41407a64e677438ee79c3ccb68c913
Quentin-Clement <quentin.clement@algosup.com>
2024-06-14 16:47:50 +0200

    fixed HUGE problem

--- src/lib/routes.dart
+++ src/lib/routes.dart
@@ -32,7 +32,7 @@ import 'package:adopte_1_candidat/chat/chat.dart';
 
 final routeProvider = Provider<GoRouter>((ref) {
   final router = GoRouter(
-    initialLocation: '/loading',
+    initialLocation: '/signup',
     routes: [
       GoRoute(
         path: '/loading',
